6be4e80713264fe256cfcba6dbaf0e5e48e9f770
Vignesh T.V <vignesh@timecampus.com>
2021-07-06 11:40:11 +0000

    Added comments in index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Fastify from 'fastify';
 import { buildApp } from './app';
 
+// Create a fastify instance in the server
 
 const app = Fastify({
     logger: true,
 });
+
+// Register the app and the relevant modules within - Our modules directory has an index.ts file which inturn loads all the relevant modules we need for the app
 
 app.register(
     buildApp({
@@ -14,11 +17,15 @@ app.register(
     }).fastifyPlugin
 );
 
+// Listen for the ready event and exit if error occured
+
 app.ready(err => {
     if (!err) return;
   
     console.error(err);
     process.exit(1);
 });
+
+// Start listening to the GraphQL Server in the port
 
 app.listen(process.env.PORT || 3000);
